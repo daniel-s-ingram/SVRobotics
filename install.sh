6c9caef5c4a2252559dac83ea21a89dae3ccdd07
Daniel Ingram <ingramds@appstate.edu>
2018-06-24 14:22:42 -0400

    Required software

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ pip install --user scipy
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get - y upgrade
 sudo apt-get -y install ros-kinetic-desktop-full
 sudo rosdep init
 rosdep update
