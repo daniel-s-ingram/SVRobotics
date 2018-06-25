fa633ff079083527d54589239cae6cf0a74992d6
Daniel Ingram <ingramds@appstate.edu>
2018-06-25 09:46:13 -0400

    Required software

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ pip install --user scipy
 
 #Install and configure ROS Kinetic
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install ros-kinetic-desktop-full
